8bdaa8cb5328d82f5b3fcbdb65b7b65681304981
soleme <soleme@gmail.com>
2021-11-01 22:03:06 +0900

    2021.11.01 강의

---
+++ day_1/3.number.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+print(1+1) #2
+print(4-2) #2
+print(4*4) #16
+print(4/2) #2
+print(pow(3,2)) #9
+print(math.sqrt(16))
+print(math.floor(random.random()*100))
+
+print(random.randrange(100,200))
+# print(round(random.random()*100))
