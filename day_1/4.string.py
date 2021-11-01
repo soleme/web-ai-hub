401a725aa7553b52119cfc3c676a8311009703ae
Leo Park <soleme@gmail.com>
2021-11-01 22:26:52 +0900

    Add files via upload

---
+++ day_1/4.string.py
@@ -0,0 +1,13 @@
+# 문자열 STRING
+print('Hello World')
+print("Hello World")
+print("Hello \nWorld") # newline (\n)
+print("Hello \"W\"orld")
+print("""
+H
+E
+L
+L
+O
+""")
+print('Hi, Hell World'.replace('Hell', 'Hello')) #문자치환
