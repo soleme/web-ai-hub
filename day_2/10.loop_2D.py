eddf1bafc403d47a92c9b3f05cfa0848a79afd91
soleme <soleme@gmail.com>
2021-11-02 21:42:23 +0900

    다차원 리스트 처리 방법

---
+++ day_2/10.loop_2D.py
@@ -0,0 +1,9 @@
+members = [
+    ['egoing','Seoul','Web'],
+    ['leezhe','Jeju','Design']
+]
+print(members[0][1])
+
+for member in members:
+    # print(member)
+    print(member[0], member[1], member[2])
