8c04952328e4ee54f96b0145ac98257612b8d831
soleme <soleme@gmail.com>
2021-11-02 20:56:56 +0900

    조건문을 이용해서 로그인 구현

---
+++ day_2/8.login.py
@@ -0,0 +1,13 @@
+id = input('id ?')
+# if id == 'egoing':
+#     print('Welcome')
+
+# if id != 'egoing':
+#     print('WHO ARE YOU!!?')
+
+if id == 'egoing':
+    print('Welcome')
+elif id == 'leezche':
+    print('Welcome')
+else :
+    print('WHO ARE YOU!!?')
