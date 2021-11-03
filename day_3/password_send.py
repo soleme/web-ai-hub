045bec68678043c4a950c3a902b756aa7594cd13
soleme <soleme@gmail.com>
2021-11-03 19:47:31 +0900

    비밀번호 발송 메일 프로그램

---
+++ day_3/password_send.py
@@ -0,0 +1,14 @@
+## 비밀번호 발송 메일 프로그램
+
+print('회원 정보를 입력하세요')
+userName = input('이름 : ')
+userMail = input('메일 : ')
+userId = input('아이디 : ')
+userPw = input('비밀번호 : ')
+
+print('To. ' + userMail)
+print('▶아이디 및 비밀번호 확인')
+print(userName + ' 고객님 안녕하세요.')
+print('고객님의 아이디와 비밀번호는 아래와 같습니다.')
+print('아이디 : ' + userId)
+print('비밀번호 : ' + userPw)
